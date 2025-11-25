62ed0ad0cb55bef0f4d6cfe406a4cbc673be20a0
Shotrush <flipyfun503@gmail.com>
2025-11-25 15:12:13 -0500

    Simple feature

--- src/main/java/org/shotrush/atom/content/systems/CombatListener.java
+++ src/main/java/org/shotrush/atom/content/systems/CombatListener.java
@@ -1,10 +1,12 @@
 package org.shotrush.atom.content.systems;
 
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.shotrush.atom.core.api.annotation.RegisterSystem;
+import org.shotrush.atom.core.util.ActionBarManager;
 
 @RegisterSystem(
         id = "combat_listener",
@@ -16,9 +18,31 @@ public class CombatListener implements Listener {
     @EventHandler
     public void onPlayerAttack(EntityDamageByEntityEvent e) {
         if (e.getDamager() instanceof Player player) {
-            int currentFoodLevel = player.getFoodLevel();
-            if (currentFoodLevel > 0) {
-                player.setFoodLevel(currentFoodLevel - 1);
+            if (player.getFoodLevel() <= 0) {
+                e.setCancelled(true);
+                ActionBarManager.send(player, "You're too hungry to fight!");
+                return;
+            }
+            float currentSaturation = player.getSaturation();
+            if (currentSaturation > 0) {
+                player.setSaturation(Math.max(0, currentSaturation - 0.5f));
+            } else {
+                player.setFoodLevel(Math.max(0, player.getFoodLevel() - 1));
+            }
+        }
+        if (e.getDamager() instanceof Projectile projectile) {
+            if (projectile.getShooter() instanceof Player shooter) {
+                if (shooter.getFoodLevel() <= 0) {
+                    e.setCancelled(true);
+                    ActionBarManager.send(shooter, "You're too hungry to fight!");
+                    return;
+                }
+                float currentSaturation = shooter.getSaturation();
+                if (currentSaturation > 0) {
+                    shooter.setSaturation(Math.max(0, currentSaturation - 0.5f));
+                } else {
+                    shooter.setFoodLevel(Math.max(0, shooter.getFoodLevel() - 1));
+                }
             }
         }
     }
